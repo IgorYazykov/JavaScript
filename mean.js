af79ce426c35438eb83b5ac6d4ce692ef226c497
Igor <gwscdf110@gmail.com>
2022-11-04 01:09:11 +0200

    saving middle

---
+++ mean.js
@@ -0,0 +1,5 @@
+firstNumber = +prompt (`Введите первое число`);
+secondNumber = +prompt (`Введите второе число`);
+thirdNumber = +prompt (`Введите третье число`);
+mean = ((firstNumber + secondNumber + thirdNumber )/3);
+alert(`среднее арифметическое число: ${mean}`);
