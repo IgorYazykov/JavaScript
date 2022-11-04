48b19c90a79b2789b0fe7f6f4661d734efa8b68a
Igor <gwscdf110@gmail.com>
2022-11-04 09:15:39 +0200

    reseving

--- mean.js
+++ mean.js
@@ -1,5 +1,13 @@
 firstNumber = +prompt (`Введите первое число`);
 secondNumber = +prompt (`Введите второе число`);
 thirdNumber = +prompt (`Введите третье число`);
-mean = ((firstNumber + secondNumber + thirdNumber )/3);
-alert(`среднее арифметическое число: ${mean}`);
+if( firstNumber == 0 && secondNumber == 0 && thirdNumber == 0){
+    alert(`вы не ввели все числа или ввели 0`)
+} 
+else if ( isNaN(firstNumber) == false && isNaN(secondNumber) == false && isNaN(thirdNumber) == false ) {
+    mean = ((firstNumber + secondNumber + thirdNumber )/3);
+    alert(`среднее арифметическое число: ${mean}`);
+}
+else{
+    alert(`вы ввели не число`)
+}
