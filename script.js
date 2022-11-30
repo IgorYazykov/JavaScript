44f7c75afe24181a15efdcbcbeb3ecd17273137b
Igor <gwscdf110@gmail.com>
2022-11-30 08:58:50 +0200

    second some fix

--- script.js
+++ script.js
@@ -9,16 +9,8 @@ console.log(NewKey);
 function generateKey (length, characters){
     let key = '';
     for (i=0; i< length ; i++){
-        key += characters[Math.ceil(Math.random() * characters.length)];
+        key += characters[Math.floor(Math.random() * characters.length)];
+        console.log(key)
     }
     return key;
 }
-
-
-// let a = 3 
-//  function addTwo(x) { 
-//  let ret = x + 2 
-//  return ret 
-//  } 
-//  let b = addTwo(a) 
-//  console.log(b)
