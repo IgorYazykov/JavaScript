1cd4e4f2cae0a9a052163c02890c9ccd8353424a
Igor <gwscdf110@gmail.com>
2022-11-30 08:51:19 +0200

    some fix

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-let key = '';
 do{
     length = prompt(`Введите количество символов в ключе`);
 } while (isNaN(length));
 
-generateKey (length, characters);
-console.log(key);
+let NewKey = generateKey (length, characters);
+console.log(NewKey);
 
 function generateKey (length, characters){
+    let key = '';
     for (i=0; i< length ; i++){
         key += characters[Math.ceil(Math.random() * characters.length)];
     }
     return key;
 }
+
+
+// let a = 3 
+//  function addTwo(x) { 
+//  let ret = x + 2 
+//  return ret 
+//  } 
+//  let b = addTwo(a) 
+//  console.log(b)
