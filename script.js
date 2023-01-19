f48683afa3439f04407c16341efa64ee196b061b
Igor <gwscdf110@gmail.com>
2023-01-19 15:11:17 +0200

    delete comments and some fix

--- script.js
+++ script.js
@@ -9,13 +9,11 @@ let finalLink = "";
 button.addEventListener('click',function(e){
     valueInput = inputForm.value;
     valueInput =[...valueInput];
-    console.log(valueInput);
     for(let i=0; i<5 ; i++){
         if (valueInput[i] == controlValue[i] || valueInput[i] == secondControlValue[i]){
             continue;
         } else {
             valueInput = [...secondControlValue, ...valueInput];
-            console.log(valueInput);
             break;
         };
     };
